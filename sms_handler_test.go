cd104dcf03e3d7d1bea0368214d5a1b16291ada3
meifakun <run_mei@163.com>
2014-12-25 16:00:53 +0800

    skip unit test of sms handler

--- sms_handler_test.go
+++ sms_handler_test.go
@@ -9,9 +9,14 @@ import (
 var (
 	phone_number = flag.String("phone_numbers", "1222", "the phone number")
 	sms_content  = flag.String("sms_content", "恒维tpt软件", "the message")
+	sms_skipped  = flag.Bool("sms_skipped", true, "the message")
 )
 
 func TestSMSHandler(t *testing.T) {
+	if *sms_skipped {
+		t.Skip("sms is skip")
+	}
+
 	handler, e := newHandler(nil, map[string]interface{}{"type": "sms",
 		"phone_numbers": *phone_number,
 		"content":       *sms_content})
