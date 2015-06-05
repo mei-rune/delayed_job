1f8b590046a4f478493ed2818c797c2ec8bbce5e
runner.mei <runner.mei@gmail.com>
2015-06-05 19:09:23 +0800

    fix compile error

--- weixin_handler_test.go
+++ weixin_handler_test.go
@@ -7,25 +7,25 @@ import (
 )
 
 var (
-	weixin_corp_id            = flag.String("weixin_corp_id", "", "")
-	weixin_corp_secret        = flag.String("weixin_corp_secret", "", "")
-	weixin_weixin_target_type = flag.String("weixin_target_type", "user", "")
-	weixin_targets            = flag.String("weixin_targets", "", "")
-	weixin_agent_id           = flag.String("weixin_agent_id", "1", "")
+	weixin_corp_id     = flag.String("weixin_corp_id", "", "")
+	weixin_corp_secret = flag.String("weixin_corp_secret", "", "")
+	weixin_target_type = flag.String("weixin_target_type", "user", "")
+	weixin_targets     = flag.String("weixin_targets", "", "")
+	weixin_agent_id    = flag.String("weixin_agent_id", "1", "")
 )
 
 func TestWeixinHandler(t *testing.T) {
-	if "" == *corp_id {
+	if "" == *weixin_corp_id {
 		t.Skip("weixin is skipped.")
 	}
 
 	handler, e := newWeixinHandler(nil, map[string]interface{}{"type": "weixin",
 		"corp_id":     *weixin_corp_id,
 		"corp_secret": *weixin_corp_secret,
-		"target_type": weixin_target_type,
-		"targets":     weixin_targets,
+		"target_type": *weixin_target_type,
+		"targets":     *weixin_targets,
 		"content":     "this is test message.",
-		"agent_id":    weixin_agent_id})
+		"agent_id":    *weixin_agent_id})
 	if nil != e {
 		t.Error(e)
 		// if e.Error() != test.excepted_error {
