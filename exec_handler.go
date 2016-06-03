70c037436f535bf926d2eda0de67fbcb06db5ce2
runner.mei <runner.mei@gmail.com>
2016-06-03 16:16:59 +0800

    enhance exec handler

--- exec_handler.go
+++ exec_handler.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 	"sync"
@@ -22,6 +23,7 @@ type execHandler struct {
 	prompt         string
 	command        string
 	arguments      []string
+	environments   []string
 }
 
 func newExecHandler(ctx, params map[string]interface{}) (Handler, error) {
@@ -35,6 +37,7 @@ func newExecHandler(ctx, params map[string]interface{}) (Handler, error) {
 	work_directory := stringWithDefault(params, "work_directory", *default_directory)
 	prompt := stringWithDefault(params, "prompt", "")
 	command := stringWithDefault(params, "command", "")
+	environments := stringsWithDefault(params, "environments", ";", nil)
 	if 0 == len(command) {
 		return nil, errors.New("'command' is required.")
 	}
@@ -54,6 +57,17 @@ func newExecHandler(ctx, params map[string]interface{}) (Handler, error) {
 		if nil != e {
 			return nil, e
 		}
+		prompt, e = genText(prompt, args)
+		if nil != e {
+			return nil, e
+		}
+		for idx, s := range environments {
+			s, e = genText(s, args)
+			if nil != e {
+				return nil, e
+			}
+			environments[idx] = s
+		}
 	}
 
 	arguments, e := shellwords.Split(command)
@@ -61,13 +75,95 @@ func newExecHandler(ctx, params map[string]interface{}) (Handler, error) {
 		return nil, errors.New("split shell command failed, " + e.Error())
 	}
 
-	return &execHandler{work_directory: work_directory, prompt: prompt, command: arguments[0], arguments: arguments[1:]}, nil
+	return &execHandler{work_directory: work_directory,
+		prompt:       prompt,
+		command:      arguments[0],
+		arguments:    arguments[1:],
+		environments: environments}, nil
+}
+
+func newExecHandler2(ctx, params map[string]interface{}) (Handler, error) {
+	if nil == ctx {
+		return nil, errors.New("ctx is nil")
+	}
+	if nil == params {
+		return nil, errors.New("params is nil")
+	}
+
+	work_directory := stringWithDefault(params, "work_directory", *default_directory)
+	prompt := stringWithDefault(params, "prompt", "")
+	environments := stringsWithDefault(params, "environments", ";", nil)
+	command := stringWithDefault(params, "command", "")
+	if 0 == len(command) {
+		return nil, errors.New("'command' is required.")
+	}
+
+	var arguments []string
+	if args, ok := params["arguments"]; ok {
+		var e error
+		arguments, e = toStrings(args)
+		if nil != e {
+			return nil, e
+		}
+	}
+
+	if args, ok := params["options"]; ok {
+		args = preprocessArgs(args)
+
+		if props, ok := args.(map[string]interface{}); ok {
+			if _, ok := props["self"]; !ok {
+				props["self"] = params
+				defer delete(props, "self")
+			}
+		}
+
+		var e error
+		command, e = genText(command, args)
+		if nil != e {
+			return nil, e
+		}
+		prompt, e = genText(prompt, args)
+		if nil != e {
+			return nil, e
+		}
+
+		for idx, s := range arguments {
+			s, e = genText(s, args)
+			if nil != e {
+				return nil, e
+			}
+			arguments[idx] = s
+		}
+
+		for idx, s := range environments {
+			s, e = genText(s, args)
+			if nil != e {
+				return nil, e
+			}
+			environments[idx] = s
+		}
+	}
+
+	return &execHandler{work_directory: work_directory,
+		prompt:       prompt,
+		command:      command,
+		arguments:    arguments,
+		environments: environments}, nil
 }
 
 func (self *execHandler) Perform() error {
 	fmt.Println(self.command, self.arguments)
 	cmd := exec.Command(self.command, self.arguments...)
 	cmd.Dir = self.work_directory
+
+	var environments []string
+	if len(self.environments) > 0 {
+		os_env := os.Environ()
+		environments = make([]string, 0, len(self.arguments)+len(os_env))
+		environments = append(environments, os_env...)
+		environments = append(environments, self.environments...)
+		cmd.Env = environments
+	}
 
 	if 0 == len(self.prompt) {
 		return cmd.Start()
@@ -135,4 +231,6 @@ func (self *execHandler) Perform() error {
 func init() {
 	Handlers["exec"] = newExecHandler
 	Handlers["exec_command"] = newExecHandler
+	Handlers["exec2"] = newExecHandler2
+	Handlers["exec2_command"] = newExecHandler2
 }
