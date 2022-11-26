4a6ab1d74308d749528973b15c3e909bfbf4a5df
meifakun <runner.mei@gmail.com>
2022-11-26 17:14:15 +0800

    add AesECBPKCS5PaddingBase64Encrypt

--- web_handler.go
+++ web_handler.go
@@ -2,6 +2,8 @@ package delayed_job
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
@@ -671,6 +673,8 @@ var Funcs = template.FuncMap{
 		return values[len(values)-1]
 	},
 
+	"AesECBPKCS5PaddingBase64Encrypt": AesECBPKCS5PaddingBase64Encrypt,
+
 	"decrypt": func(s string) string {
 		return Decrypt(s)
 	},
@@ -803,4 +807,102 @@ func IsZero(v reflect.Value) bool {
 func IsZeroValue(value interface{}) bool {
 	v := reflect.ValueOf(value)
 	return IsZero(v)
+}
+
+// func AesDecrypt(crypted, key []byte) []byte {
+//     block, err := aes.NewCipher(key)
+//     if err != nil {
+//     	panic(err)
+//     }
+//     blockMode := NewECBDecrypter(block)
+//     origData := make([]byte, len(crypted))
+//     blockMode.CryptBlocks(origData, crypted)
+//     origData = PKCS5UnPadding(origData)
+//     return origData
+// }
+
+func AesECBPKCS5PaddingBase64Encrypt(src, key string) string {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		panic(err)
+	}
+	if src == "" {
+		panic("plain content empty")
+	}
+	ecb := NewECBEncrypter(block)
+	content := []byte(src)
+	content = PKCS5Padding(content, block.BlockSize())
+	crypted := make([]byte, len(content))
+	ecb.CryptBlocks(crypted, content)
+	// 普通base64编码加密 区别于urlsafe base64
+	return base64.StdEncoding.EncodeToString(crypted)
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	// 去掉最后一个字节 unpadding 次
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+type ecb struct {
+	b         cipher.Block
+	blockSize int
+}
+
+func newECB(b cipher.Block) *ecb {
+	return &ecb{
+		b:         b,
+		blockSize: b.BlockSize(),
+	}
+}
+
+type ecbEncrypter ecb
+
+// NewECBEncrypter returns a BlockMode which encrypts in electronic code book
+// mode, using the given Block.
+func NewECBEncrypter(b cipher.Block) cipher.BlockMode {
+	return (*ecbEncrypter)(newECB(b))
+}
+func (x *ecbEncrypter) BlockSize() int { return x.blockSize }
+func (x *ecbEncrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("crypto/cipher: input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("crypto/cipher: output smaller than input")
+	}
+	for len(src) > 0 {
+		x.b.Encrypt(dst, src[:x.blockSize])
+		src = src[x.blockSize:]
+		dst = dst[x.blockSize:]
+	}
+}
+
+type ecbDecrypter ecb
+
+// NewECBDecrypter returns a BlockMode which decrypts in electronic code book
+// mode, using the given Block.
+func NewECBDecrypter(b cipher.Block) cipher.BlockMode {
+	return (*ecbDecrypter)(newECB(b))
+}
+func (x *ecbDecrypter) BlockSize() int { return x.blockSize }
+func (x *ecbDecrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%x.blockSize != 0 {
+		panic("crypto/cipher: input not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("crypto/cipher: output smaller than input")
+	}
+	for len(src) > 0 {
+		x.b.Decrypt(dst, src[:x.blockSize])
+		src = src[x.blockSize:]
+		dst = dst[x.blockSize:]
+	}
 }
